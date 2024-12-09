ad838f5871556455c2a7efa24cf79fe01d1df325
Muhammadilhamakb <111202214109@mhs.dinus.ac.id>
2024-12-09 13:14:47 +0700

    goals dan statisiic

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignInPage from "./pages/signIn";
+import SignUpPage from "./pages/signUp";
+import ErrorRoute from "./pages/errorRoute";
+import ForgotPassword from "./pages/forgotPassword";
+import DashboardPage from "./pages/dashboard";
+import BalancePage from "./pages/balance";
+import ExpensePage from "./pages/expense";
+
+const App = () => {
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <DashboardPage/>,
+      errorElement: <ErrorRoute/>,
+    },
+    {
+      path: "/eror",
+      element: <ErrorRoute/>
+    },
+    {
+      path: "/login",
+      element: <SignInPage/>
+    },
+    {
+      path: "/register",
+      element: <SignUpPage/>
+    },
+    {
+      path: "/forgot-password",
+      element: <ForgotPassword/>
+    },
+    {
+      path: "/balance",
+      element: <BalancePage/>
+    },
+    {
+      path: "/expense",
+      element: <ExpensePage/>
+    }
+
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={myRouter} />
+    </>
+  );
+};
+
+export default App;
